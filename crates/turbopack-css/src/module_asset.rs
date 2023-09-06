3a07081d082a7b96b1b5067bf6e2941942943aa0
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-06 15:20:08 +0900

    More

--- crates/turbopack-css/src/module_asset.rs
+++ crates/turbopack-css/src/module_asset.rs
@@ -31,7 +31,7 @@ use turbopack_ecmascript::{
 };
 
 use crate::{
-    process::{ProcessCss, ProcessCssResult},
+    process::{FinalCssResult, ProcessCss, ProcessCssResult},
     references::{compose::CssModuleComposeReference, internal::InternalCssAssetReference},
 };
 
@@ -158,7 +158,7 @@ impl ModuleCssAsset {
         let mut classes = IndexMap::default();
 
         // TODO(alexkirsz) Should we report an error on parse error here?
-        if let ProcessCssResult::Ok {
+        if let FinalCssResult::Ok {
             exports: Some(exports),
             ..
         } = &*result
