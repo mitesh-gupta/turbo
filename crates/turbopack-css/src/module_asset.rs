f1735d04d9a8d9a1e517b8f142e0149c2442c099
Donny/강동윤 <kdy1997.dev@gmail.com>
2023-09-06 15:39:04 +0900

    Rename

--- crates/turbopack-css/src/module_asset.rs
+++ crates/turbopack-css/src/module_asset.rs
@@ -31,7 +31,7 @@ use turbopack_ecmascript::{
 };
 
 use crate::{
-    process::{FinalCssResult, ProcessCss, ProcessCssResult},
+    process::{FinalCssResult, ParseCssResult, ProcessCss},
     references::{compose::CssModuleComposeReference, internal::InternalCssAssetReference},
 };
 
